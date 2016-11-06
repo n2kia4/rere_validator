2ce6afc377bd7cb294e1e7350fbadd4c73d62411
Natsuki Tanaka <natsukia3@gmail.com>
2016-11-06 18:16:24 +0900

    Add validation for reserved words

--- lib/rere_validator.rb
+++ lib/rere_validator.rb
@@ -1,5 +1,26 @@
 require "rere_validator/version"
 require "rere_validator/reserved_words"
-module RereValidator
-  # Your code goes here...
+
+class RegexReservedValidator < ActiveModel::EachValidator
+  DEFAULT_MESSAGE = {
+    :reserved_words => "can't be used a reserved words"
+  }.freeze
+
+  def validate_each(record, attribute, value)
+    if reserved?(value)
+      add_error(record, attribute, :reserved_words)
+    end
+  end
+
+  def add_error(record, attr_name, message)
+    message_options = {
+      default: [DEFAULT_MESSAGE[message], options[:message]],
+      scope: [:errors, :messages]
+    }
+    record.errors.add(attr_name, I18n.t(message, message_options))
+  end
+
+  def reserved?(value)
+    RESERVED_WORDS.include?(value.downcase)
+  end
 end
