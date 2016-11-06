8c852dab9a0d223be398a197a5ebd92cf732bb11
Natsuki Tanaka <natsukia3@gmail.com>
2016-11-06 19:06:59 +0900

    Add validation for regex

--- lib/rere_validator.rb
+++ lib/rere_validator.rb
@@ -3,12 +3,23 @@ require "rere_validator/reserved_words"
 
 class RegexReservedValidator < ActiveModel::EachValidator
   DEFAULT_MESSAGE = {
+    :invalid_characters => "can't contain invalid characters",
+    :invalid_characters_at_begin => "can't begin with '.', '-', '_'",
+    :invalid_characters_at_end => "can't end with '.', '-', '_'",
     :reserved_words => "can't be used a reserved words"
   }.freeze
 
   def validate_each(record, attribute, value)
     if reserved?(value)
       add_error(record, attribute, :reserved_words)
+    end
+
+    if value !~ /\A[a-zA-Z0-9_.-]+\z/
+      add_error(record, attribute, :invalid_characters)
+    elsif value.match(/\A[_.-].*\z/)
+      add_error(record, attribute, :invalid_characters_at_begin)
+    elsif value.match(/\A.*[_.-]\z/)
+      add_error(record, attribute, :invalid_characters_at_end)
     end
   end
 
