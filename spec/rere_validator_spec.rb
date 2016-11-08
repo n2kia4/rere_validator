72952e042856ecf64e4b6b2c33d67f549d433710
Natsuki Tanaka <natsukia3@gmail.com>
2016-11-08 20:14:56 +0900

    Write tests using RSpec

--- spec/rere_validator_spec.rb
+++ spec/rere_validator_spec.rb
@@ -1,11 +1,50 @@
 require 'spec_helper'
 
 describe RereValidator do
-  it 'has a version number' do
-    expect(RereValidator::VERSION).not_to be nil
+  describe 'regex' do
+    it 'returns valid when match the regex' do
+      expect('foo_bar').to match(/\A[a-zA-Z0-9_.-]+\z/)
+    end
+
+    it 'returns invalid when not match the regex' do
+      expect('foo&bar').to_not match(/\A[a-zA-Z0-9_.-]+\z/)
+    end
+
+    context 'when begin with invalid characters' do
+      ['.foo', '-foo', '_foo'].each do |username|
+        it "returns invalid for #{username}" do
+          expect(username).to match(/\A[_.-].*\z/)
+        end
+      end
+    end
+
+    context 'when end with invalid characters' do
+      ['bar.', 'bar-', 'bar_'].each do |username|
+        it "returns invalid for #{username}" do
+          expect(username).to match(/\A.*[_.-]\z/)
+        end
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'reserved' do
+    it 'returns valid when not included in reserved words' do
+      expect(RESERVED_WORDS).to_not include('foobar')
+    end
+
+    it 'returns invalid when included in reserved words' do
+      expect(RESERVED_WORDS).to include('admin')
+    end
+
+    context 'when included in add reserved words' do
+      before do
+        @user = UserWithAddReservedWords.new
+      end
+
+      it 'returns invalid' do
+        @user.username = 'foobar'
+        expect(@user).to_not be_valid
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
+require 'active_model'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rere_validator'
+
+class UserWithAddReservedWords
+  include ActiveModel::Validations
+
+  attr_accessor :username
+
+  validates :username, regex_reserved: {
+    add_reserved_words: %w[foobar]
+  }
+end
